ddf26d8e85083e2f0a3000a06257fed038062632
Sebastian Smith <Sebastian Smith>
2016-07-05 19:38:35 -0500

    working code

--- factorial.py
+++ factorial.py
@@ -1,11 +1,12 @@
 def factorial(x):
-    factor = 0
+    factor = 1
     if x > 0:
-        for each in range(x):
-            if x == 1:
-                factor = x * 1
-            else:
-                factor = x * (each - 1)
+        if x == 1:
+            factor = x * 1
+        else:
+            while x > 0:
+                factor = factor * x
+                x = x-1
         return factor
     else:
         print "not a positive number"
